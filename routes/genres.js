c3d00329cfc97480329085c771f802fd7cda42ef
b-45 <bernard11ade@gmail.com>
2018-07-24 12:17:09 -0400

    route handler to DELETE genre

--- routes/genres.js
+++ routes/genres.js
@@ -33,24 +33,34 @@ router.post('/', async (req, res) => {
   res.send(genre);
 });
 
-//PUT route handler
+// PUT route handler
 
 router.put('/:id', async (req, res) => {
+  // validate genre before updating database
   const {
     error
   } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // update database
   const genre = await Genre.findByIdAndUpdate(req.params.id, {
     name: req.body.name
   }, {
     new: true
   });
-
+  // check if there is no genre 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
 });
+
+// Delete
+router.delete('/:id', async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id)
+  if (!genre) return res.status(404).send('The genre with the given ID was not found')
+  res.send(genre)
+})
+
 
 
 
