0f0c8761ee33b2f4d61ef7ec6b8221b7c35222a5
b-45 <bernard11ade@gmail.com>
2018-07-27 12:15:54 -0400

    created route handler for GET movie by ID

--- routes/movies.js
+++ routes/movies.js
@@ -13,3 +13,9 @@ router.get('/', async (req, res) => {
   const movies = await Movie.find().sort('name')
   res.send(movies)
 })
+
+router.get('/:id', async (req, res) => {
+  const movie = await Movie.findById(req.params.id)
+  if (!movie) return res.status(404).send('There movie with given ID was not found')
+  res.send(movie)
+})
