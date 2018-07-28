ae980530554ec6a61c3693bead95aaa7b493242f
b-45 <bernard11ade@gmail.com>
2018-07-28 09:46:08 -0400

    created route handler for UPDATE  movie

--- routes/movies.js
+++ routes/movies.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const movie = await Movie.findById(req.params.id)
-  if (!course) return res.status(404).send("The movie with the given ID was not found")
+  if (!movie) return res.status(404).send("The movie with the given ID was not found")
   res.send(movie)
 })
 
@@ -24,10 +24,10 @@ router.post('/', async (req, res) => {
   const {
     error
   } = validate(req.body)
-  if (error) return res.status(400).send(error.detail[0].message)
+  if (error) return res.status(400).send(error.details[0].message)
 
   const genre = await Genre.findById(req.body.genreId)
-  if (!genre) return res.status(400).send("Invalid genre")
+  if (!genre) return res.status(400).send('Invalid genre')
 
   let movie = new Movie({
     title: req.body.title,
@@ -39,5 +39,29 @@ router.post('/', async (req, res) => {
     dailyRentalRate: req.body.dailyRentalRate
   })
   movie = await movie.save()
+  res.send(movie)
+})
+
+router.put('/:id', async (req, res) => {
+  const {
+    error
+  } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const genre = await Genre.findById(req.body.genreId)
+  if (!genre) return res.status(400).send('Invalid Genre')
+
+  const movie = await Movie.findByIdAndUpdate(req, params.id, {
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate
+  }, {
+    new: true
+  })
+  if (!movie) return res.status(404).send('The movie with given Id is not found')
   res.send(movie)
 })
