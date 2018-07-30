3909c3a032bf4fdaf7a71c85ebb53892bf5ac4dd
b-45 <bernard11ade@gmail.com>
2018-07-30 14:30:33 -0400

    create route handler for GET rentals

---
+++ routes/rental.js
@@ -0,0 +1,11 @@
+const {Rental, validate} = require('../models/rental'); 
+const {Movie} = require('../models/movie'); 
+const {Customer} = require('../models/customer'); 
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut');
+  res.send(rentals);
+});
